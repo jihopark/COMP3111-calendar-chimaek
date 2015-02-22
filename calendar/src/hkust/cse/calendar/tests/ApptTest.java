36f9bcbd4a2dce581be77b2c73ab1dd1fe663f08
jihopark <lastspurt17@gmail.com>
2015-02-22 15:48:54 +0800

    Created hkust.cse.calendar.tests package & added sample ApptTest

---
+++ calendar/src/hkust/cse/calendar/tests/ApptTest.java
@@ -0,0 +1,14 @@
+package hkust.cse.calendar.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApptTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
