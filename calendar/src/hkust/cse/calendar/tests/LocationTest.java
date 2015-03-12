dd7aeccbc2e6108485a5920ef29e58e652713f66
Jimmy Kim <jkimab@ust.hk>
2015-03-12 11:39:11 +0800

    Created LocationTest #2
    Created unit testing for Location

---
+++ calendar/src/hkust/cse/calendar/tests/LocationTest.java
@@ -0,0 +1,26 @@
+package hkust.cse.calendar.tests;
+
+import static org.junit.Assert.*;
+import hkust.cse.calendar.unit.Location;
+
+import org.junit.Test;
+
+public class LocationTest {
+	
+	@Test
+	public void locationShouldHaveRequiredFields() {
+		Location location = new Location("location",0);
+		assertFalse("locatoin does not have required fields", location.isValid());
+	}
+	
+	@Test
+	public void locationNameShouldNotBeTooLong() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void locationIDShouldNotBeNegetive() {
+		fail("Not yet implemented");
+	}
+	
+}
