c5ed629529465dfe1f1bd6eb27ff31d7277c212d
wotomas <jkimab@ust.hk>
2015-03-14 18:15:39 +0800

    Modify notification class #14
    modify structure of notification

--- calendar/src/hkust/cse/calendar/unit/Notification.java
+++ calendar/src/hkust/cse/calendar/unit/Notification.java
@@ -1,23 +1,47 @@
 package hkust.cse.calendar.unit;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+
 public class Notification {
 	private String _name;
-	private int _reminderHour;
+	private LinkedList<Date> _alarmTime;
+	private LinkedList<Boolean> _alarmFlag;
+	
+	private Date _savedTime;
 	private int _notificationID;
 	
-	public Notification(String name, int reminderHour ) {
-		_name = name;
-		_reminderHour = reminderHour;
+	// A default constructor
+	public Notification() {
+		_name = "";
 		_notificationID = 0;
+		_alarmTime = new LinkedList<Date>();
+		_alarmFlag = new LinkedList<Boolean>();
+		_savedTime = Calendar.getInstance().getTime();
+
 	}
 	
+	public Notification(Notification notification ) {
+		_name = notification.getName();
+		_notificationID = 0;
+		_alarmFlag = notification.getFlags();
+		_alarmTime = notification.getAlarms();
+	}
+
+
+	private LinkedList<Date> getAlarms() {
+		return _alarmTime;
+	}
+
+	private LinkedList<Boolean> getFlags() {
+		return _alarmFlag;
+	}
+
 	public String getName() {
 		return _name;
 	}
 	
-	public int getReminderHour() {
-		return _reminderHour;
-	}
 	public int getID() {
 		return _notificationID;
 	}
@@ -26,13 +50,19 @@ public class Notification {
 		_name = name;
 	}
 		
-	public void setReminderHour(int reminderHour) {
-		_reminderHour = reminderHour;
-	}
-
 	public void setID(int id) {
 		_notificationID = id;
 	}
+	
+	private void setAlarms(LinkedList<Boolean> flags) {
+		//check flag1, set alarm1 repeat 4 times
+	}
+	
+	private void setFlags(boolean firstFlag, boolean secondFlag, boolean thirdFlag, boolean fourthFlag) {
+		//if flag1 2 3 4.. set _alarmFlag
+	}
+	
+	
 	
 	public String toString() {
 		return _name;
