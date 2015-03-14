47e5601a3c7692ced8c532479eedd6be3b746996
Jimmy Kim <jkimab@ust.hk>
2015-03-15 01:34:57 +0800

    Added new thead check to check for Notification
    Added new thead check to check for Notification every period of time

--- calendar/src/hkust/cse/calendar/Main/CalendarMain.java
+++ calendar/src/hkust/cse/calendar/Main/CalendarMain.java
@@ -2,17 +2,27 @@
 package hkust.cse.calendar.Main;
 
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+
 import javax.swing.UIManager;
 
 import hkust.cse.calendar.gui.LoginDialog;
+import hkust.cse.calendar.unit.Notification;
+import hkust.cse.calendar.unit.User;
+import hkust.cse.calender.notificationstorage.NotificationController;
+import hkust.cse.calender.notificationstorage.NotificationStorageNullImpl;
 
 
 public class CalendarMain {
 	public static boolean logOut = false;
-	
+	public static Date currentTime = new Date();
+	public static User user = new User( "noname", "nopass");
 	public static void main(String[] args) {
 		while(true){
 			logOut = false;
+			//testForNotification();
 			try{
 		//	UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 			}catch(Exception e){
@@ -22,6 +32,15 @@ public class CalendarMain {
 			while(logOut == false){
 				try {
 					Thread.sleep(300);
+					currentTime = Calendar.getInstance().getTime();
+					
+					//System.out.println(currentTime);
+					if(NotificationController.getInstance().checkForNotificationTime(currentTime)) {
+						Notification notification = NotificationController.getInstance().retrieveNotification(currentTime);
+						System.out.println("Yes we have an notification at: " + currentTime);
+						System.out.println("Notification name is: " + notification.getName());
+						
+					}
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -29,5 +48,36 @@ public class CalendarMain {
 			}
 		}
 	}
+	
+	
+	private static void testForNotification() {
+		// only for testing
+		NotificationController.getInstance().initNotificationStorage(new NotificationStorageNullImpl(user));
+		Notification notification = new Notification();
+		notification.setName("Test");
+		notification.setID(0);
+
+		//future notification for testing
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(Calendar.getInstance().getTime());
+		cal.add(Calendar.SECOND, 5);
+		cal.add(Calendar.HOUR, 1);
+		Date Future = cal.getTime();
+		
+		System.out.println("Future Time is : " + Future);
+		
+		notification.setAppointmentTime(Future);
+		notification.setFlags(true,true,false,false);
+		notification.setAlarms();
+		NotificationController.getInstance().saveNewNotification(notification);
+		for(Date d : NotificationController.getInstance().retrieveAllNotificationTimes(notification)) {
+			System.out.println("Saved Notification Times are : " + d);
+		}
+		LinkedList<Date> alarms = notification.getAlarms();
+		for(Date d : alarms) {
+			System.out.println("Alarm is set at: " + d);
+		}
+	}
+
 }
 		
